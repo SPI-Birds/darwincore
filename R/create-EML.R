0d8b0f1c80bd92edf8c33e9bb7ae540cba42fff3
Cherine Jantzen <cherine.jantzen@gmail.com>
2024-05-17 10:58:44 +0200

    Add project, data & R folders and existing scripts with ouputs

---
+++ R/create-EML.R
@@ -0,0 +1,170 @@
+# Pipeline for creation of EML metadata file of the DwC-A of the SPI Birds NIOO data ####
+
+# Authors: Cherine Jantzen
+# Created: 2024-05-14
+# Last updated: 2024-05-16
+
+
+# Load packages
+library(emld)
+library(EML)
+library(xml2)
+library(here)
+
+# now the script requires the taxonInformation object from the mapping script
+load(here::here("data", "taxonInformation.rda"))
+load(here::here("data", "locationInformation.rda"))
+
+
+# I. Fill in metadata ----------------------------------------------------
+
+# Title of the data set
+title <- "Breeding pair abundances derived from nestbox studies of the Netherlands Institute of Ecology (NIOO-KNAW)"
+
+# Information on the creator of the data set
+creator <- list(individualName = list(givenName = "Marcel",
+                                      surName = "Visser"),
+                organizationName = "Department of Animal Ecology, Netherlands Institute of Ecology (NIOO-KNAW)",
+                address = list(country = "NL",
+                               city = "Wageningen"),
+                electronicMailAddress = "m.visser@nioo.knaw.nl",
+                userId = "0000-0002-1456-1939")
+
+# Information on the provider of the meta data
+metadataProvider <- list(individualName = list(givenName = "Cherine",
+                                               surName = "Jantzen"),
+                         organizationName = "Netherlands Institute of Ecology (NIOO-KNAW)",
+                         address = list(country = "NL",
+                                        city = "Wageningen"),
+                         electronicMailAddress = "c.jantzen@nioo.knaw.nl",
+                         userId = "0009-0006-0723-2682")
+
+# Information on the contact person
+contact_person <- list(organizationName = "SPI-Birds",
+                       address = list(country = "NL", # FIXME Where?
+                                      city = "Wageningen"), # FIXME Where?
+                       positionName = "?", # FIXME Who?
+                       electronicMailAddress = "spibirds@nioo.knaw.nl") # FIXME correct?
+
+# Language of the data
+language <- "en"
+
+# Abstract describing the data set
+abstract <- list(para = "??")
+
+                   
+# List of keywords and the thesaurus they are listed in
+keywords <- list(list(keyword = list("breeding birds", "ecology", "bird species abundance", "phenology"),
+                      keywordThesaurus = "envThes"),
+                 list(keyword = list("nest box", "passerine")))
+
+# License for the work
+licensed <- list(licenseName = "Creative Commons Attribution 4.0 International (CC BY 4.0)",
+                 url = "https://creativecommons.org/licenses/by/4.0/")
+
+# geographic coverage
+geographic_coverage <- list(geographicDescription = "Eight study sites across the Netherlands, namely Hoge Veluwe, Vlieland, Liesbosch Breda, Westerheide, Buunderkamp, Lichtenbeek, Oosterhout and Warnsborn.",
+                            boundingCoordinates = list(westBoundingCoordinate = as.character(min(locationInformation$decimalLongitude)),
+                                                       eastBoundingCoordinate = as.character(max(locationInformation$decimalLongitude)),
+                                                       northBoundingCoordinate = as.character(max(locationInformation$decimalLatitude)),
+                                                       southBoundingCoordinate = as.character(min(locationInformation$decimalLatitude))))
+# Temporal coverage of the data
+temporal_coverage <- list(rangeOfDates = list(beginDate = list(calendarDate = "1955"),
+                                              endDate = list(calendarDate = "2023")))
+
+# Taxonomic coverage of the data
+taxonomicClassification <- purrr::map(.x = 1:nrow(taxonInformation),
+                                      .f = ~{
+                                        
+                                        taxonList <- list(taxonRankName = "Species",
+                                                          taxonRankValue = taxonInformation$species[.x],
+                                                          taxonId = taxonInformation$specieskey[.x],
+                                                          commonName = taxonInformation$vernacularName[.x])
+                                      })
+
+taxonomic_coverage <- list(generalTaxonomicCoverage = "Data is collected for 8 bird species.",
+                           taxonomicClassification = taxonomicClassification)
+
+# Combine all three types of coverage
+coverage <- list(geographicCoverage = geographic_coverage,
+                 temporalCoverage = temporal_coverage,
+                 taxonomicCoverage = taxonomic_coverage) 
+
+# Methods for data collection
+methods <- list(methodStep = list(list(description = list(para = "Data is collected by regular visits of nest boxes. ...")),
+                                  list(description = list(para = "The original breeding data is transformed into the SPI-Birds standard format through a SPI-Birds pipeline. ....")),
+                                  list(description = list(para = "From the data in the SPI-Birds format, the number of breeding pairs is calculated per year, location and species. For this, the number of first clutches is counted. ")))) # TODO
+
+# Maintenance: frequency of updates
+maintenance <- list(maintenanceUpdateFrequency = "unknown",
+                    description = list(para = "Frequency of data updates is unknown."))
+
+# Project information
+project <- list(title = "SPI-Birds",
+                personnel = list(individualName = list(givenName = "name", # FIXME
+                                                       surName = "name"),
+                                 organizationName = "organisation",
+                                 address = list(country = "country",
+                                                city = "city"),
+                                 electronicMailAddress = "spibirds@nioo.knaw.nl",
+                                 role = "person"))
+
+
+# Create EML file ---------------------------------------------------------
+
+packageId <- "d8370484-e2bd-483d-a1c5-9a87fc625793"
+
+# Combine all components in one list
+eml <- list(dataset =
+              list(title = title,
+                   creator = creator,
+                   metadataProvider = metadataProvider,
+                   language = language,
+                   abstract = abstract,
+                   keywordSet = keywords,
+                   licensed = licensed,
+                   coverage = coverage,
+                   maintenance = maintenance,
+                   contact = contact_person,
+                   methods = methods,
+                   project = project),
+            system = "uuid",
+            packageId = packageId)
+
+# Write EMl file
+EML::write_eml(eml, file = here::here("data", "SPIBirds_EML.xml"))
+
+
+# 3. Add attributes for specific nodes ------------------------------------
+
+# Read EML file as XML file
+EML <- xml2::read_xml(here::here("data", "SPIBirds_EML.xml"))
+
+# Identify all taxonId nodes for which attribute shall be set
+taxonId_node <- xml2::xml_find_all(EML, xpath = "//taxonId")
+
+# Set "provider" attribute for taxonId nodes
+xml2::xml_set_attr(taxonId_node, attr = "provider", value = "https://www.gbif.org/")
+
+# Identify title node
+title_node <- xml2::xml_find_first(EML, xpath = "//title")
+
+# Set title attribute
+xml2::xml_set_attr(title_node, attr = "xml:lang", value = "en")
+
+# Identify userId node
+userId_node <- xml2::xml_find_all(EML, xpath = "//userId")
+
+# Set directory attribute
+xml2::xml_set_attr(userId_node, attr = "directory", value = "https://orcid.org")
+
+
+# 4. Validate EML file ----------------------------------------------------
+if(!emld::eml_validate(EML)) {
+  
+  stop("The generated EML is not schema-valid.")
+  
+}
+
+# Write final EML file
+xml2::write_xml(EML, file = here::here("data", "SPIBirds_EML.xml"))
---
+++ R/function-map-to-DwC-A.R
@@ -0,0 +1,131 @@
+# Function to map SPI Birds derived abundance data on breeding pairs to Darwin Core Archive with occurrence core ####
+
+# Author: Cherine Jantzen
+# Created: 2024-05-15
+# Last updated: 2024-05-17
+
+
+# I. Preparation ----------------------------------------------------------
+
+## load packages
+library(dplyr)
+library(taxize)
+library(stringr)
+library(tidyr)
+library(here)
+
+# II. Function ------------------------------------------------------------
+
+# Arguments
+
+## countryCode: character, two letter ISO country code specifying the country in which the data was collected
+## institution: character, specifying the name of the institution that owns the data
+## institutionID: character, specifying the institional ID as required for DwC-term "institutionID". Should ideally be Research Organization Registry (ROR) identifier (e.g., "https://ror.org/01g25jp36")
+## quantityType: character, specifying the system or unit in which the organism Quantities are given (i.e. DwC-term "organismQuantityType"). Default is set to "breeding pairs" as the function is tailored to breeding pair count data
+## output_prefix: character, specifying the prefix of the filename for the output csv file
+
+map_to_DwCA <- function(countryCode,
+                        institution,
+                        institutionID,
+                        quantityType = "breeding pairs",
+                        output_prefix) {
+  
+  # get supportive information to translate species and location codes from SPI Birds & get coordinates of study sites
+  species_codes <- read.csv("https://raw.githubusercontent.com/SPI-Birds/pipelines/master/inst/extdata/species_codes.csv")
+  pop_codes <- read.csv("https://raw.githubusercontent.com/SPI-Birds/pipelines/master/inst/extdata/pop_codes.csv")
+  pop_locations <- read.csv("https://raw.githubusercontent.com/SPI-Birds/pipelines/master/inst/extdata/pop_locations.csv")
+  
+  # A. Taxonomic information ----
+  
+  ## find unique species in the data and translate species codes to binomial names
+  taxa <- tibble::tibble(SpeciesID = unique(breedingPairs$Species)) %>%
+    dplyr::left_join(species_codes %>% 
+                       dplyr::select("Species", "BinomialName", "CommonName"), 
+                     by = c("SpeciesID" = "Species"))
+  
+  ## query taxonomic information from GBIF
+  taxonInformation <- taxize::get_gbifid_(sci = taxa$BinomialName) %>% 
+    dplyr::bind_rows() %>% 
+    dplyr::filter(status == "ACCEPTED", matchtype == "EXACT") %>% 
+    dplyr::left_join(taxa, by = c("canonicalname" = "BinomialName")) %>% 
+    tidyr::separate(canonicalname, c("Genus", "specificEpithet"), remove = FALSE) %>% 
+    dplyr::mutate(taxonRank = "species") %>% 
+    dplyr::rename("vernacularName" = "CommonName")
+  
+  # B. Location information ----
+  
+  ## translate population ID into verbatim names and get coordinates per area
+  locationInformation <- brood %>% 
+    dplyr::distinct(., PopID, .keep_all = FALSE) %>% 
+    dplyr::left_join(pop_codes %>% 
+                       dplyr::select("PopID", "PopName"), 
+                     by = "PopID") %>% 
+    dplyr::mutate(verbatimLocality = dplyr::case_when(PopID == "LIC" ~ "Lichtenbeek", 
+                                                      PopID == "LIE" ~ "Liesbosch Breda", 
+                                                      TRUE ~ PopName)) %>% 
+    dplyr::left_join(pop_locations %>% 
+                       dplyr::select("site_name", "country", "latitude", "longitude") %>% 
+                       dplyr::mutate(site_name = dplyr::case_when(stringr::str_detect(string = site_name, pattern = "Westerheide") ~ "Westerheide",
+                                                                  TRUE ~ site_name),
+                                     countryCode = countryCode),
+                     by = c("verbatimLocality" = "site_name")) %>% 
+    dplyr::rename(decimalLatitude = latitude,
+                  decimalLongitude = longitude) %>% 
+    dplyr::mutate(geodeticDatum = "EPSG:4326")
+  
+  # C. Get Event information ----
+  
+  # event date range
+  eventInformation <- brood %>% 
+    dplyr::group_by(PopID, Species, BreedingSeason) %>% 
+    dplyr::summarise(minLD = min(LayDate_min),
+                     maxLD = max(LayDate_max)) %>% 
+    dplyr::mutate(eventDate = dplyr::if_else(!is.na(minLD), paste(minLD, substring(maxLD, first = 6, last = 10), sep = "/"), as.character(BreedingSeason))) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::left_join(location %>% 
+                       dplyr::distinct(., PopID, HabitatType, .keep_all = FALSE),
+                     by = "PopID") %>% 
+    # add metadata fields of event class
+    dplyr::mutate(language = "en",
+                  institutionID = institutionID,
+                  institutionCode = institution)
+  
+  # D. Combine information into occurrence core file ----
+  occurrence <- breedingPairs %>% 
+    # add taxonomic information
+    dplyr::left_join(taxonInformation %>% 
+                       dplyr::select("SpeciesID", "kingdom", "phylum", "class", "order", "family", "genus", 
+                                     "specificEpithet", "scientificName" = "scientificname", "taxonRank", "vernacularName"),
+                     by = c("Species" = "SpeciesID")) %>% 
+    # add location information
+    dplyr::left_join(locationInformation %>% 
+                       dplyr::select(!"PopName"), 
+                     by = "PopID") %>% 
+    # add event information
+    dplyr::left_join(eventInformation %>% 
+                       dplyr::select(!c("minLD", "maxLD")), 
+                     by = c("PopID", "Species", "BreedingSeason")) %>% 
+    dplyr::rename("year" = "BreedingSeason",
+                  "organismQuantity" = "n") %>% 
+    # add missing terms
+    dplyr::mutate(basisOfRecord = "HumanObservation",
+                  organismQuantityType = quantityType,
+                  occurrenceStatus = "present",
+                  occurrenceID = paste(PopID, year, Species, sep = "-")) %>% 
+    # reorder columns for the final output file
+    dplyr::select("occurrenceID", "year", "eventDate", "organismQuantity", "organismQuantityType", "occurrenceStatus", "basisOfRecord",
+                  "country", "countryCode", "verbatimLocality", "decimalLatitude", "decimalLongitude", "geodeticDatum", "language", 
+                  "institutionID", "institutionCode", "scientificName", "kingdom", "phylum", "class", "order", "family", "genus", 
+                  "specificEpithet", "taxonRank", "vernacularName")
+  
+  # save additional information to be used in EML file
+  save(taxonInformation, file = here::here("data", "taxonInformation.rda"))
+  save(locationInformation, file = here::here("data", "locationInformation.rda"))
+  
+  # save occurrence output file
+  # write occurrence file
+  write.csv(occurrence, file = here::here("data", paste(output_prefix, "occurrence.csv", sep = "_")), row.names = FALSE)
+  
+  return(occurrence)
+  
+}
