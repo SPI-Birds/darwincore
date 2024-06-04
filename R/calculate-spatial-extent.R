d369ae99dfdf9e73493f470e375d036d84377503
Stefan Vriend <svriend@gmail.com>
2024-06-04 11:56:02 +0200

    Add function to calculate spatial extent of study sites
    used to determine dwc:coordinateUncertaintyInMeters, which was advised by Jeroen (NLBIF)

---
+++ R/calculate-spatial-extent.R
@@ -0,0 +1,93 @@
+# Function to calculate the spatial extent of a study location ####
+
+# Authors: Stefan Vriend
+# Created: 2024-06-04
+# Last updated: 2024-06-04
+
+
+# Load packages -----------------------------------------------------------
+
+library(dplyr)
+library(geosphere)
+library(leaflet)
+
+# Function ----------------------------------------------------------------
+
+# Arguments:
+# - brood: R data object containing SPI-Birds formatted brood data
+# - location: R data object containing SPI-Birds formatted location data
+
+calculate_spatial_extent <- function(brood,
+                                     location) {
+  
+  # Keep location records that are associated with broods
+  location <- location |> 
+    dplyr::filter(LocationID %in% brood$LocationID)
+  
+  # Calculate the four margins of the spatial bounding box  (S, N, W, E) 
+  extent <- location |>
+    dplyr::summarise(minLat = min(Latitude, na.rm = TRUE),
+                     maxLat = max(Latitude, na.rm = TRUE),
+                     minLon = min(Longitude, na.rm = TRUE),
+                     maxLon = max(Longitude, na.rm = TRUE),
+                     .by = "PopID")
+  
+  # Extract centre points from pop_codes (a SPI-Birds internal table)
+  centroids <- pop_codes |> 
+    dplyr::filter(PopID %in% extent$PopID) |> 
+    dplyr::select(PopID, Latitude, Longitude)
+  
+  # Calculate the distances between the margins and centre points.
+  # The maximum distance describes the smallest circle containing all relevant locations,
+  # and provides a good measure of dwc:coordinateUncertaintyInMeters
+  dplyr::left_join(extent, centroids, by = "PopID") |> 
+    dplyr::rowwise(PopID) |> # Because geosphere::distHaversine() is not vectorised
+    dplyr::mutate(west = geosphere::distHaversine(p1 = c(minLon, Latitude),
+                                                  p2 = c(Longitude, Latitude)),
+                  east = geosphere::distHaversine(p1 = c(maxLon, Latitude),
+                                                  p2 = c(Longitude, Latitude)),
+                  north = geosphere::distHaversine(p1 = c(Longitude, maxLat),
+                                                   p2 = c(Longitude, Latitude)),
+                  south = geosphere::distHaversine(p1 = c(Longitude, minLat),
+                                                   p2 = c(Longitude, Latitude))) |> 
+    dplyr::mutate(uncertainty = round(max(c(west, east, north, south))),
+                  uncertainty = dplyr::if_else(uncertainty == 0, NA_real_, uncertainty)) |> 
+    dplyr::select("PopID", 
+                  "coordinateUncertaintyInMeters" = "uncertainty")
+  
+}
+
+# Example
+# calculate_spatial_extent(brood, location)
+
+
+# Function to verify locations on an interactive map ----------------------
+
+# Arguments:
+# - PopID: SPI-Birds identifier of study site 
+# - brood: R data object containing SPI-Birds formatted brood data
+# - location: R data object containing SPI-Birds formatted location data
+
+leaflet_map <- function(PopID,
+                        brood,
+                        location) {
+  
+  # Keep location records that are associated with broods
+  location <- location |> 
+    dplyr::filter(LocationID %in% brood$LocationID)
+  
+  # Map individual location records to map
+  # Check for potentially erroneous records
+  leaflet::leaflet() |> 
+    leaflet::fitBounds(lng1 = min(location[location$PopID == PopID, ]$Longitude),
+                       lng2 = max(location[location$PopID == PopID, ]$Longitude),
+                       lat1 = min(location[location$PopID == PopID, ]$Latitude),
+                       lat2 = max(location[location$PopID == PopID, ]$Latitude)) |> 
+    leaflet::addTiles() |> 
+    leaflet::addCircleMarkers(lng = location[location$PopID == PopID, ]$Longitude,
+                              lat = location[location$PopID == PopID, ]$Latitude)
+  
+}
+
+# Example
+# leaflet_map("HOG", brood, location)
