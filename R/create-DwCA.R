2e816578bd0e263b5db99e1eac5eaa2c698d9167
Cherine Jantzen <cherine.jantzen@gmail.com>
2024-06-04 14:37:11 +0200

    Add event core and adapt and update scripts & files accordingly, add protocol as function argument and incorporate coordinateUncertainty

--- R/create-DwCA.R
+++ R/create-DwCA.R
@@ -19,6 +19,9 @@ location <- read.csv(file.choose()) # Location_data
 source(here::here("R","function-map-to-DwC-A.R"))
 source("https://raw.githubusercontent.com/LTER-LIFE/FDFDT/main/R/create-meta-xml-of-DwCA.R")
 
+# retrieve location information for each population from SPI Birds
+pop_codes <- read.csv("https://raw.githubusercontent.com/SPI-Birds/pipelines/master/inst/extdata/pop_codes.csv")
+
 # II. Data processing ---------------------------------------------------------
 
 ## calculate species abundance as breeding pairs based on number of first clutches using ClutchType_calculated
@@ -30,13 +33,25 @@ breedingPairs <- brood %>%
 
 # III. Map to Darwin Core -------------------------------------------------
 
-occurrence <- map_to_DwCA(institution = "NIOO-KNAW",
-                          institutionID = "https://ror.org/01g25jp36",
-                          data_directory = "data",
-                          output_prefix = "SPIBirds")
+map_to_DwCA(protocol = "Visser, M. E., Lindner, M., Gienapp, P., Long, M. C., & Jenouvrier, S. (2021). 
+            Recent natural variability in global warming weakened phenological mismatch and selection on seasonal timing in great tits (Parus major). 
+            Proceedings of the Royal Society B, 288(1963), 20211337. https://doi.org/10.1098/rspb.2021.1337", 
+            institution = "NIOO-KNAW",
+            institutionID = "https://ror.org/01g25jp36",
+            data_directory = "data",
+            output_prefix = "SPIBirds") %>% 
+  purrr::map2(.x = c("event", "occurrence"),
+              .y = .,
+              .f = ~ {
+                
+                assign(.x, .y, envir = .GlobalEnv)
+                
+              })
+  
 
 # IV. Meta-xml file -------------------------------------------------------
 
 # create meta.xml file for beech crop DwC-A
-create_meta_xml(core = c("Occurrence" = here::here("data", "SPIBirds_occurrence.csv")),
+create_meta_xml(core = c("Event" = here::here("data", "SPIBirds_event.csv")),
+                extensions = c("Occurrence" = here::here("data", "SPIBirds_occurrence.csv")),
                 file = here::here("data", ("SPIBirds_meta.xml")))
--- R/function-map-to-DwC-A.R
+++ R/function-map-to-DwC-A.R
@@ -2,7 +2,7 @@
 
 # Authors: Cherine Jantzen, Stefan Vriend
 # Created: 2024-05-15
-# Last updated: 2024-05-30
+# Last updated: 2024-06-04
 
 
 # I. Preparation ----------------------------------------------------------
@@ -12,18 +12,21 @@ library(dplyr)
 library(taxize)
 library(stringr)
 library(tidyr)
+library(lubridate)
 
 # II. Function ------------------------------------------------------------
 
 # Arguments
 
+## protocol: character, specifying the sampling protocol used to collect the data
 ## institution: character, specifying the name of the institution that owns the data
 ## institutionID: character, specifying the institional ID as required for DwC-term "institutionID". Should ideally be Research Organization Registry (ROR) identifier (e.g., "https://ror.org/01g25jp36")
 ## quantityType: character, specifying the system or unit in which the organism Quantities are given (i.e. DwC-term "organismQuantityType"). Default is set to "breeding pairs" as the function is tailored to breeding pair count data
 ## data_directory: character, specifying the name of the folder or directory the output files should be stored in
 ## output_prefix: character, specifying the prefix of the filename for the output csv file
 
-map_to_DwCA <- function(institution,
+map_to_DwCA <- function(protocol,
+                        institution,
                         institutionID,
                         quantityType = "breeding pairs",
                         data_directory,
@@ -31,7 +34,10 @@ map_to_DwCA <- function(institution,
   
   # get supportive information to translate species and location codes from SPI Birds & get coordinates of study sites
   species_codes <- read.csv("https://raw.githubusercontent.com/SPI-Birds/pipelines/master/inst/extdata/species_codes.csv")
-  pop_codes <- read.csv("https://raw.githubusercontent.com/SPI-Birds/pipelines/master/inst/extdata/pop_codes.csv")
+  # pop_codes <- read.csv("https://raw.githubusercontent.com/SPI-Birds/pipelines/master/inst/extdata/pop_codes.csv")
+  
+  # get function to calculate the geographic uncertainty
+  source(here::here("R", "calculate-spatial-extent.R"))
   
   # A. Taxonomic information ----
   
@@ -53,6 +59,7 @@ map_to_DwCA <- function(institution,
     dplyr::rename("vernacularName" = "CommonName")
   
   # B. Location information ----
+  coordinateUncertainty <- calculate_spatial_extent(brood, location)
   
   ## get location information from SPI-Birds file `pop_codes`
   locationInformation <- brood %>% 
@@ -66,9 +73,18 @@ map_to_DwCA <- function(institution,
     dplyr::mutate(geodeticDatum = "EPSG:4326",
                   continent = "Europe",
                   decimalLatitude = round(Latitude, digits = 5),
-                  decimalLongitude = round(Longitude, digits = 5))
+                  decimalLongitude = round(Longitude, digits = 5)) %>% 
+    dplyr::left_join(coordinateUncertainty, by = "PopID")
   
   # C. Get Event information ----
+  
+  # calculate number of nest boxes sampled per year-location to take as sampleSizeValue
+  sampleSize <- brood %>% 
+    dplyr::filter(ClutchType_calculated == "first") %>% 
+    dplyr::group_by(PopID, Species, BreedingSeason) %>% 
+    dplyr::distinct(LocationID, .keep_all = TRUE) %>% 
+    dplyr::summarise(sampleSizeValue = dplyr::n()) %>% 
+    dplyr::ungroup()
   
   # event date range
   eventInformation <- brood %>% 
@@ -80,47 +96,66 @@ map_to_DwCA <- function(institution,
                   startDayOfYear = lubridate::yday(substring(eventDate, first = 1, last = 10)),
                   endDayOfYear = lubridate::yday(substring(eventDate, first = 12, last = 21))) %>% 
     dplyr::ungroup() %>% 
-    # add metadata fields of event class
+    dplyr::left_join(sampleSize, by = c("PopID", "BreedingSeason", "Species")) %>% 
+    dplyr::rename("year" = "BreedingSeason") %>% 
+      # add metadata fields of event class
     dplyr::mutate(language = "en",
                   institutionID = institutionID,
-                  institutionCode = institution)
+                  institutionCode = institution,
+                  samplingProtocol = protocol, 
+                  sampleSizeUnit = "nest boxes",
+                  type = "Event",
+                  eventID = paste(PopID, year, Species, sep = "-"))
   
-  # D. Combine information into occurrence core file ----
+  # D. Combine information into event core & occurrence extension file ----
+  
+  ## Event core
+  event <- eventInformation %>% 
+    # add location information
+    dplyr::left_join(locationInformation, by = "PopID") %>% 
+    dplyr::select("eventID", "samplingProtocol", "sampleSizeValue", "sampleSizeUnit", "eventDate", "startDayOfYear", 
+                  "endDayOfYear", "year",  "continent", "country", "countryCode", "decimalLatitude","decimalLongitude",
+                  "geodeticDatum", "coordinateUncertaintyInMeters", "verbatimLocality", "type", "language", "institutionID", "institutionCode")
+  
+  
+  ## Occurrence extension
   occurrence <- breedingPairs %>% 
+    left_join(eventInformation %>%  
+                dplyr::select("PopID", "year", "Species", "eventID"),
+              by = c("PopID", "BreedingSeason" = "year", "Species")) %>% 
     # add taxonomic information
     dplyr::left_join(taxonInformation %>%
                        dplyr::select("SpeciesID", "kingdom", "phylum", "class", "order", "family", "genus",
                                      "specificEpithet", "scientificName" = "scientificname", "taxonRank", "vernacularName", "genericName", "taxonomicStatus"),
                      by = c("Species" = "SpeciesID")) %>%
-    # add location information
-    dplyr::left_join(locationInformation, 
-                     by = "PopID") %>% 
-    # add event information
-    dplyr::left_join(eventInformation %>% 
-                       dplyr::select(!c("minLD", "maxLD")), 
-                     by = c("PopID", "Species", "BreedingSeason")) %>% 
-    dplyr::rename("year" = "BreedingSeason",
-                  "organismQuantity" = "n") %>% 
+    dplyr::rename("organismQuantity" = "n") %>% 
     # add missing terms
     dplyr::mutate(basisOfRecord = "HumanObservation",
                   organismQuantityType = quantityType,
                   occurrenceStatus = "present",
-                  occurrenceID = paste(PopID, year, Species, sep = "-")) %>% 
-      # reorder columns for the final output file
-    dplyr::select("occurrenceID", "year", "eventDate", "startDayOfYear", "endDayOfYear", "organismQuantity", "organismQuantityType", "occurrenceStatus", "basisOfRecord", "continent",
-                  "country", "countryCode", "verbatimLocality", "decimalLatitude", "decimalLongitude", "geodeticDatum", "language", 
-                  "institutionID", "institutionCode", "scientificName", "kingdom", "phylum", "class", "order", "family", "genus", 
+                  occurrenceID = paste(eventID, 1:dplyr::n(), sep = "_"), .by = eventID) %>% 
+    # reorder columns for the final output file
+    dplyr::select("eventID", "occurrenceID", "organismQuantity", "organismQuantityType", "occurrenceStatus", "basisOfRecord",      
+                  "scientificName", "kingdom", "phylum", "class", "order", "family", "genus", 
                   "genericName", "specificEpithet", "taxonRank", "vernacularName", "taxonomicStatus")
   
+
+  # E. Save output files ----------------------------------------------------
+
   # save additional information to be used in EML file
   save(taxonInformation, file = paste(data_directory, "taxonInformation.rda", sep = "/"))
   save(locationInformation, file = paste(data_directory, "locationInformation.rda", sep = "/"))
 
-  # save occurrence output file
+  # write event file
+  write.csv(event, file = paste(data_directory, paste(output_prefix, "event.csv", sep = "_"), sep = "/"),
+            row.names = FALSE)
+  
   # write occurrence file
   write.csv(occurrence, file = paste(data_directory, paste(output_prefix, "occurrence.csv", sep = "_"), sep = "/"),
             row.names = FALSE)
   
-  return(occurrence)
+  
+  
+  return(list(event, occurrence))
   
 }
